3b0c3403ce678d12d9a4de9fc06b16e47548f6ed
Anh Trinh <ant19@duke.edu>
2016-12-10 14:34:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, IntegerField
-from wtforms.validators import DataRequired
+from flask import Flask, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import models
+import forms
 
-class AnonymousFilterForm(FlaskForm):
-    beginDate = IntegerField('beginDate',[DataRequired()])
-    endDate =  IntegerField('endDate',[DataRequired()])
+app = Flask(__name__)
+app.secret_key = 's3cr3t'
+app.config.from_object('config')
+db = SQLAlchemy(app, session_options={'autocommit': False})
+
+
+@app.route('/',methods =['GET','POST'])
+def section():
+    form = forms.AnonymousFilterForm()
+    return render_template('edit-drinker.html', 
+                           title='Main Page',
+                           form=form)
+
+@app.template_filter('pluralize')
+def pluralize(number, singular='', plural='s'):
+    return singular if number in (0, 1) else plural
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
