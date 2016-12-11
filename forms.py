0833a0252e1e971f2ba38c1c8282f4b1a8f5acf6
Anh Trinh <ant19@duke.edu>
2016-12-11 11:58:28 -0500

    Update forms.py

--- forms.py
+++ forms.py
@@ -3,5 +3,9 @@ from wtforms import StringField, BooleanField, IntegerField
 from wtforms.validators import DataRequired
 
 class AnonymousFilterForm(FlaskForm):
-    beginDate = IntegerField('beginDate',[DataRequired()])
-    endDate =  IntegerField('endDate',[DataRequired()])
+    beginDay = IntegerField('beginDay',[DataRequired()],default = 1)
+    endDay =  IntegerField('endDay',[DataRequired()],default = 28)
+    beginMonth = IntegerField('beginMonth',[DataRequired()],default = 1)
+    endMonth =  IntegerField('endMonth',[DataRequired()],default = 12)
+    beginYear = IntegerField('beginYear',[DataRequired()],default = 2000)
+    endYear =  IntegerField('endYear',[DataRequired()],default = 2016)
