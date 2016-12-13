66855e2a42913943513fc1dbebed965cf6f3e94d
chiarui424 <chiarui424@gmail.com>
2016-12-12 22:30:14 -0500

    Update load.sql

--- anonsdb/load.sql
+++ anonsdb/load.sql
@@ -1,2 +1,2 @@
-\COPY Anon(title,link,agency,day,month,year,phrase,section,snip) FROM './data/sample_nytimes16years.csv' WITH DELIMITER ',' CSV
+\COPY Anon(title,link,agency,day,month,year,phrase,section,snip,fulldate) FROM './data/sample_nytimes16years.csv' WITH DELIMITER ',' CSV
 
