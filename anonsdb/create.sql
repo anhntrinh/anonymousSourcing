7c8c6a3abf66ce406964a0f8c128c69461772873
Anh Trinh <ant19@duke.edu>
2016-12-13 02:48:18 -0500

    Update create.sql

--- anonsdb/create.sql
+++ anonsdb/create.sql
@@ -1,3 +1,14 @@
-CREATE TABLE Anon(title VARCHAR, link VARCHAR, agency VARCHAR, day SMALLINT, month SMALLINT, year SMALLINT,phrase VARCHAR,section VARCHAR, snip VARCHAR, fulldate INT);
+CREATE TABLE Anon(
+	title VARCHAR, 
+	link VARCHAR, 
+	agency VARCHAR, 
+	day SMALLINT, 
+	month SMALLINT, 
+	year SMALLINT,
+	phrase VARCHAR,
+	section VARCHAR, 
+	snip VARCHAR,
+	pdate DATE
+);
 
 
