da136ca349ab38fe530b894c49620f8b88907261
chiarui424 <chiarui424@gmail.com>
2016-12-12 22:20:33 -0500

    Update create.sql

--- anonsdb/create.sql
+++ anonsdb/create.sql
@@ -1,3 +1,3 @@
-CREATE TABLE Anon(title VARCHAR, link VARCHAR, agency VARCHAR, day SMALLINT, month SMALLINT, year SMALLINT,phrase VARCHAR,section VARCHAR, snip VARCHAR);
+CREATE TABLE Anon(title VARCHAR, link VARCHAR, agency VARCHAR, day SMALLINT, month SMALLINT, year SMALLINT,phrase VARCHAR,section VARCHAR, snip VARCHAR, fulldate date);
 
 
