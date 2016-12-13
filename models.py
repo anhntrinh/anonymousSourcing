7fce646aa1eb1ddf39db0654153f8d5a95618d48
Anh Trinh <ant19@duke.edu>
2016-12-13 02:43:50 -0500

    Update models.py

--- models.py
+++ models.py
@@ -1,18 +1,19 @@
 from sqlalchemy import sql, orm
 from app import db
 
+
 class BigTable(db.Model):
     __tablename__ = 'anon'
-    link = db.Column('link', db.Text, primary_key=True)
     title = db.Column('title', db.Text) 
+    link = db.Column('link', db.Text, primary_key=True)
     agency = db.Column('agency',db.Text)
     day  = db.Column('day', db.Integer)
     month = db.Column('month', db.Integer)
     year = db.Column('year', db.Integer)
     phrase = db.Column('phrase',db.Text)
     section = db.Column('section',db.Text)
-    snip = db.Column('snip',db.Text)
-    fulldate = db.Column('fulldate',db.date)
+    snip = db.Column('snip',db.Text, primary_key=True)
+    pdate = db.Column('pdate',db.Date)
 
     
 
