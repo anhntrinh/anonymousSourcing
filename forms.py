a1ce4059157bc5638c3849a9316a95faa1fc4f8e
Anh Trinh <ant19@duke.edu>
2016-12-13 02:46:38 -0500

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,13 +1,25 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, IntegerField, SelectMultipleField, SelectField, DateField
+from wtforms import IntegerField, SelectMultipleField, SelectField, DateField
 from wtforms.validators import DataRequired
+from datetime import datetime,date
 
-import datetime
+
 
 class AnonymousFilterForm(FlaskForm):
-    beginDate = DateField('Start Date', format='%m/%d/%Y', default=datetime.date(2000,1,1))
-    endDate = DateField('End Date', format='%m/%d/%Y',default = datetime.date.today())
+    '''
+    beginDay = IntegerField('beginDay',[DataRequired()],default = 1)
+    endDay =  IntegerField('endDay',[DataRequired()],default = 28)
+    beginMonth = IntegerField('beginMonth',[DataRequired()],default = 1)
+    endMonth =  IntegerField('endMonth',[DataRequired()],default = 12)
+    beginYear = IntegerField('beginYear',[DataRequired()],default = 2000)
+    endYear =  IntegerField('endYear',[DataRequired()],default = 2016)
+    '''
+
+    beginDate = DateField('beginDate', format='%Y-%m-%d', default=date(2000,01,01))
+    endDate = DateField('endDate', format='%Y-%m-%d',default = date(2016,10,10))
     sectionMenu = SelectMultipleField('sectionMenu',choices=[],coerce = int)
+    scale = SelectField('scale',choices=[(1,'Month'), (2,'Year')],coerce = int,default = [1])
+
     
     def __init__(self,sectionQuery):
         super(AnonymousFilterForm, self).__init__()
