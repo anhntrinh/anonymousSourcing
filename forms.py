d839bb188e76c25a2a63541ef79f82535022d7ef
Anh Trinh <ant19@duke.edu>
2016-12-13 18:42:10 -0500

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,24 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, SelectMultipleField, SelectField, DateField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, optional
 from datetime import datetime,date
 
-
-
 class AnonymousFilterForm(FlaskForm):
-    '''
-    beginDay = IntegerField('beginDay',[DataRequired()],default = 1)
-    endDay =  IntegerField('endDay',[DataRequired()],default = 28)
-    beginMonth = IntegerField('beginMonth',[DataRequired()],default = 1)
-    endMonth =  IntegerField('endMonth',[DataRequired()],default = 12)
-    beginYear = IntegerField('beginYear',[DataRequired()],default = 2000)
-    endYear =  IntegerField('endYear',[DataRequired()],default = 2016)
-    '''
 
-    beginDate = DateField('beginDate', format='%Y-%m-%d', default=date(2000,01,01))
-    endDate = DateField('endDate', format='%Y-%m-%d',default = date(2016,10,10))
+    beginDate  = DateField('beginDate', format = '%m/%d/%Y', default = date(2000,01,01))
+    endDate = DateField('endDate', format='%m/%d/%Y',default = date(2016,10,10))
     sectionMenu = SelectMultipleField('sectionMenu',choices=[],coerce = int)
-    scale = SelectField('scale',choices=[(1,'Month'), (2,'Year')],coerce = int,default = [1])
+    scale = SelectField('scale',[optional()],choices=[(1,'Month'), (2,'Year')],coerce = int,default = [1])
 
     
     def __init__(self,sectionQuery):
