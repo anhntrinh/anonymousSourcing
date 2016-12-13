c747cbea01750d8ef423f809d6041bff6a779db3
chiarui424 <chiarui424@gmail.com>
2016-12-12 22:34:10 -0500

    Add files via upload

--- forms.py
+++ forms.py
@@ -1,15 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, IntegerField, SelectMultipleField, SelectField
+from wtforms import StringField, BooleanField, IntegerField, SelectMultipleField, SelectField, DateField
 from wtforms.validators import DataRequired
 
+import datetime
 
 class AnonymousFilterForm(FlaskForm):
-    beginDay = IntegerField('beginDay',[DataRequired()],default = 1)
-    endDay =  IntegerField('endDay',[DataRequired()],default = 28)
-    beginMonth = IntegerField('beginMonth',[DataRequired()],default = 1)
-    endMonth =  IntegerField('endMonth',[DataRequired()],default = 12)
-    beginYear = IntegerField('beginYear',[DataRequired()],default = 2000)
-    endYear =  IntegerField('endYear',[DataRequired()],default = 2016)
+    beginDate = DateField('Start Date', format='%m/%d/%Y', default=datetime.date(2000,1,1))
+    endDate = DateField('End Date', format='%m/%d/%Y',default = datetime.date.today())
     sectionMenu = SelectMultipleField('sectionMenu',choices=[],coerce = int)
     
     def __init__(self,sectionQuery):
