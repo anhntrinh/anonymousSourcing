5fe40b399cd752de78059113eb93a39ea87ac3cc
Anh Trinh <ant19@duke.edu>
2016-12-14 00:35:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, session
+from flask import Flask, render_template, redirect, url_for, session, make_response
 from flask_sqlalchemy import SQLAlchemy
 import models
 import forms
@@ -6,6 +6,8 @@ import pygal
 from datetime import datetime,timedelta,date
 from sqlalchemy import distinct
 from collections import OrderedDict
+import csv
+
 
 app = Flask(__name__)
 app.secret_key = 's3cr3t'
@@ -63,7 +65,7 @@ def output(begin,end):
     
     print "output"
 
-    count = 0 
+    
     # import variables from previous pages
     scaleG = session.get('scaleGraph',None)
     section_choices = session.get('sectionChoices',None)
@@ -72,17 +74,42 @@ def output(begin,end):
     section_name_Chosen = [section_L[i] for i in section_choices]   # get the actual list of section names chosen
     print section_name_Chosen
 
+    section_name_Chosen_quote = [ "'"+sec+"'" for sec in section_name_Chosen]
+
     beginDate = datetime.strptime(begin, '%Y-%m-%d')
     endDate = datetime.strptime(end,'%Y-%m-%d')
 
-
+    count = 0 
     #  count of total number of anon cites in the time period 
     count = db.session.execute('SELECT COUNT(*) FROM anon WHERE pdate >= :beginDate AND pdate <= :endDate',{'beginDate': beginDate,'endDate':endDate}).first()[0]
     
+
+
+    # query for csv output
+    query = ""
+    if len(section_name_Chosen)!= 0:
+        query = "SELECT * FROM anon WHERE pdate >=" + "'"+begin+"'" +" AND pdate <=" + "'"+end+"'" +" AND section IN (" + ",".join(section_name_Chosen_quote) + ")"
+    else:   
+        query = "SELECT * FROM anon WHERE pdate >=" + "'"+begin+"'" +" AND pdate <=" + "'"+end+"'"
+    print("query " + query)
+
+    # get the csv data
     
+ 
+    '''
+    if len(section_name_Chosen)>0:
+        queryResult = db.session.execute('SELECT * FROM anon WHERE pdate >= :beginDate AND pdate <= :endDate AND section IN :sections',{'beginDate': beginDate,'endDate':endDate,'sections':section_name_Chosen})
+    else:
+        queryResult = db.session.execute('SELECT * FROM anon WHERE pdate >= :beginDate AND pdate <= :endDate',{'beginDate': beginDate,'endDate':endDate})
+'''
+    queryResult = db.session.execute('SELECT * FROM anon WHERE pdate >= :beginDate AND pdate <= :endDate',{'beginDate': beginDate,'endDate':endDate})
+    
+    #result = get_branch_data_file(queryResult)
+
+    # get the graph data
     if (beginDate.year == endDate.year) and (beginDate.month == endDate.month): # less than a month 
         graph = pygal.Bar(height = 200,width = 500)
-        graph.title = "Anonymous Sourcing"
+        graph.title = str(begin) +" to " +str(end)
         graph.x_labels = ['NYT'] if (len(section_name_Chosen) == 0) else [] # the name of section or no name 
         # adding y values into the bar graph
 
@@ -101,7 +128,7 @@ def output(begin,end):
     else: # when dates are different in months 
 
         graph=pygal.Line(height = 400)
-        graph.title = "Anonymous Sourcing"
+        graph.title = str(begin) +" to "+str(end)
         if scaleG==1:
             months = monthScale(beginDate,endDate, "%b-%Y") #list of strings of month-year 
             # add x values in the graph 
@@ -156,8 +183,48 @@ def output(begin,end):
 
     graph_data = graph.render_data_uri()
 
-    return render_template('drinker.html', count = count, graph_data=graph_data)
+    return render_template('drinker.html', count = count, graph_data=graph_data, query=query)
 
+@app.route("/getPlotCSV/<query>")
+def get_branch_data_file(query):
+    data = db.session.execute(query)
+    print "number of rows " + str(data.rowcount)
+    datal = list(data.fetchall())
+    return render_template('csvoutput.html', data=datal)
+
+    '''
+    (file_basename, server_path, file_size) = create_csv(data)
+    return_file = open(server_path+file_basename, 'r')
+    response = make_response(return_file,200)
+    response.headers['Content-Description'] = 'File Transfer'
+    response.headers['Cache-Control'] = 'no-cache'
+    response.headers['Content-Type'] = 'text/csv'
+    response.headers['Content-Disposition'] = 'attachment; filename=%s' % file_basename
+    response.headers['Content-Length'] = file_size
+    return response
+
+## function to create csv from the SQLAlchemy query response object
+
+
+def create_csv(data):
+    """ returns (file_basename, server_path, file_size) """
+    file_basename = 'output.csv'
+    server_path = '/'
+    #w_file = open(server_path+file_basename,'w+')
+    w_file =open(r"/output.csv","w+")
+    w_file.write('title,link,agency,day,month,year,phrase,section,snip,pdate \n')
+    
+    for row in data:
+        row_as_string = str(row)
+        w_file.write(row_as_string[1:-1] + '\n') ## row_as_string[1:-1] because row is a tuple
+
+    w_file.close()
+
+    w_file = open(server_path+file_basename,'r')
+    file_size = len(w_file.read())
+    return file_basename, server_path, file_size
+
+'''
 @app.template_filter('pluralize')
 def pluralize(number, singular='', plural='s'):
     return singular if number in (0, 1) else plural
