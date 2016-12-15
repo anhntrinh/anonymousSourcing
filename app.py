fe6c12bb203d9b811885c5978e0787421eb917f4
Anh Trinh <ant19@duke.edu>
2016-12-14 22:31:44 -0500

    Update app.py
    final version

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, session, make_response
+from flask import Flask, render_template, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 import models
 import forms
@@ -18,7 +18,7 @@ db = SQLAlchemy(app, session_options={'autocommit': False})
 def section():   
     # all unique sections from table 
     #sectionL = db.session.query(distinct(models.BigTable.section)).all()
-    sectione = db.session.execute('SELECT DISTINCT section FROM anon ORDER BY section')
+    sectione = executeQuery('SELECT DISTINCT section FROM anon ORDER BY section')
     sectionL = [r for r, in sectione]
     i = 0
     sec=[]
@@ -31,18 +31,13 @@ def section():
     # initiate all possible choces for section box
     form.sectionMenu.choices = sec
     
-
-    if form.validate_on_submit():
-        print "validated"
-        
-
+    if form.validate_on_submit():        
         beginDate = form.beginDate.data 
         endDate =  form.endDate.data
 
 
         # section choices
         sectionChoices = form.sectionMenu.data # list of int 
-        print sectionChoices
         # scale choice 
         scaleG = form.scale.data
 
@@ -51,11 +46,20 @@ def section():
         session['sectionList'] = sectionL
         session['scaleGraph'] = scaleG
 
-    	return redirect(url_for('output',begin = beginDate, end = endDate))
+        return redirect(url_for('output',begin = beginDate, end = endDate))
+
     return render_template('edit-drinker.html',title='Sign In',form=form)
 
+
+def executeQuery(*query):
+    if len(query)==1:
+        return db.session.execute(query[0])
+    if len(query)==2:
+        return db.session.execute(query[0],query[1])
+
+
                        
-def monthScale(start, end, filter):
+def xScale(start, end, filter):
     result = OrderedDict(((start + timedelta(_)).strftime(filter), None) for _ in xrange((end - start).days)).keys()
     return result
 
@@ -64,7 +68,6 @@ def monthScale(start, end, filter):
 def output(begin,end):
     
     print "output"
-
     
     # import variables from previous pages
     scaleG = session.get('scaleGraph',None)
@@ -72,7 +75,6 @@ def output(begin,end):
     section_L= session.get('sectionList',None)
 
     section_name_Chosen = [section_L[i] for i in section_choices]   # get the actual list of section names chosen
-    print section_name_Chosen
 
     section_name_Chosen_quote = [ "'"+sec+"'" for sec in section_name_Chosen]
 
@@ -81,30 +83,17 @@ def output(begin,end):
 
     count = 0 
     #  count of total number of anon cites in the time period 
-    count = db.session.execute('SELECT COUNT(*) FROM anon WHERE pdate >= :beginDate AND pdate <= :endDate',{'beginDate': beginDate,'endDate':endDate}).first()[0]
-    
-
-
     # query for csv output
-    query = ""
+    query = "" # for the getCSV 
+    query_count = "" # for the count variable (to get the number)
     if len(section_name_Chosen)!= 0:
         query = "SELECT * FROM anon WHERE pdate >=" + "'"+begin+"'" +" AND pdate <=" + "'"+end+"'" +" AND section IN (" + ",".join(section_name_Chosen_quote) + ")"
+        query_count = "SELECT COUNT(*) FROM anon WHERE pdate >=" + "'"+begin+"'" +" AND pdate <=" + "'"+end+"'" +" AND section IN (" + ",".join(section_name_Chosen_quote) + ")"
     else:   
         query = "SELECT * FROM anon WHERE pdate >=" + "'"+begin+"'" +" AND pdate <=" + "'"+end+"'"
-    print("query " + query)
-
-    # get the csv data
-    
- 
-    '''
-    if len(section_name_Chosen)>0:
-        queryResult = db.session.execute('SELECT * FROM anon WHERE pdate >= :beginDate AND pdate <= :endDate AND section IN :sections',{'beginDate': beginDate,'endDate':endDate,'sections':section_name_Chosen})
-    else:
-        queryResult = db.session.execute('SELECT * FROM anon WHERE pdate >= :beginDate AND pdate <= :endDate',{'beginDate': beginDate,'endDate':endDate})
-'''
-    queryResult = db.session.execute('SELECT * FROM anon WHERE pdate >= :beginDate AND pdate <= :endDate',{'beginDate': beginDate,'endDate':endDate})
-    
-    #result = get_branch_data_file(queryResult)
+        query_count = "SELECT COUNT(*) FROM anon WHERE pdate >=" + "'"+begin+"'" +" AND pdate <=" + "'"+end + "'"
+        
+    count = executeQuery(query_count).first()[0]
 
     # get the graph data
     if (beginDate.year == endDate.year) and (beginDate.month == endDate.month): # less than a month 
@@ -114,76 +103,61 @@ def output(begin,end):
         # adding y values into the bar graph
 
         if len(section_name_Chosen) == 0: 
-            countD=db.session.execute('SELECT COUNT(*) FROM anon WHERE :beginDate <= pdate AND pdate <= :endDate',{'beginDate':beginDate, 'endDate':endDate}).first()[0]
+            countD=executeQuery('SELECT COUNT(*) FROM anon WHERE :beginDate <= pdate AND pdate <= :endDate',{'beginDate':beginDate, 'endDate':endDate}).first()[0]
             graph.add('NYT',countD) 
         else:
             for sectionName in section_name_Chosen:
-                Sname = 'NYT-' + sectionName # cuztomize for each section 
-                countD=db.session.execute('SELECT COUNT(*) FROM anon WHERE :beginDate <= pdate AND pdate <= :endDate AND section =:sectionName',{'beginDate':beginDate, 'endDate':endDate,'sectionName':sectionName}).first()[0]
+                # cuztomize for each section 
+                countD=executeQuery('SELECT COUNT(*) FROM anon WHERE :beginDate <= pdate AND pdate <= :endDate AND section =:sectionName',{'beginDate':beginDate, 'endDate':endDate,'sectionName':sectionName}).first()[0]
                 # add to graph  for that section 
-
-                graph.add(Sname,countD) 
-
-
+                graph.add('NYT-' + sectionName,countD) 
     else: # when dates are different in months 
 
         graph=pygal.Line(height = 400)
         graph.title = str(begin) +" to "+str(end)
+
         if scaleG==1:
-            months = monthScale(beginDate,endDate, "%b-%Y") #list of strings of month-year 
+
+            months = xScale(beginDate,endDate, "%b-%Y") #list of strings of month-year 
             # add x values in the graph 
-            graph.x_labels= months
+
             if len(section_name_Chosen) == 0: 
-                Sname = 'NYT'
-                monthCount=[]
-                for month_year in months:
-                    date= datetime.strptime(month_year, '%b-%Y')
-                    cMonth=db.session.execute('SELECT COUNT(*) FROM anon WHERE :beginDate <= pdate AND pdate <= :endDate AND month=:monthInt AND year=:yearInt',{'monthInt':date.month,'yearInt':date.year, 'beginDate':beginDate, 'endDate':endDate}).first()[0]
-                    monthCount.append(cMonth) 
-                graph.add(Sname, monthCount) 
+                q= 'SELECT COUNT(*) FROM anon WHERE :beginDate <= pdate AND pdate <= :endDate AND month=:monthInt AND year=:yearInt'
+                constructGraph('%b-%Y',months,q,{'beginDate':beginDate, 'endDate':endDate},'NYT',graph) 
             else:
                 for sectionName in section_name_Chosen:
-                    Sname = 'NYT-' + sectionName # cuztomize for each section 
-                    monthCount=[]
-                    for month_year in months:
-                        date= datetime.strptime(month_year, '%b-%Y')
-                        cMonth=db.session.execute('SELECT COUNT(*) FROM anon WHERE :beginDate <= pdate AND pdate <= :endDate AND month=:monthInt AND year=:yearInt AND section =:sectionName',{'monthInt':date.month,'yearInt':date.year, 'beginDate':beginDate, 'endDate':endDate,'sectionName':sectionName}).first()[0]
-                        monthCount.append(cMonth)
-               
-                    # add to graph the line for that section 
-                    graph.add(Sname, monthCount) 
-              
-             
+                    q='SELECT COUNT(*) FROM anon WHERE :beginDate <= pdate AND pdate<= :endDate AND month=:monthInt AND year=:yearInt AND section =:sectionName'
+                    constructGraph('%b-%Y',months,q,{'beginDate':beginDate, 'endDate':endDate,'sectionName':sectionName},'NYT-' + sectionName,graph)
         else:
-            years = monthScale(beginDate,endDate, "%Y")
             # x values 
-            graph.x_labels= years
+            years = xScale(beginDate,endDate, "%Y")
+
             if len(section_name_Chosen) == 0: 
-                Sname = 'NYT'
-                monthCount=[]
-                for year in years:
-                    date= datetime.strptime(year,'%Y')
-                    cMonth=db.session.execute('SELECT COUNT(*) FROM anon WHERE :beginDate <= pdate AND pdate <= :endDate AND year=:yearInt',{'yearInt':date.year, 'beginDate':beginDate, 'endDate':endDate}).first()[0]
-                    monthCount.append(cMonth) 
-                graph.add(Sname, monthCount) 
-
-
-            for sectionName in section_name_Chosen:
-                Sname = 'NYT-' + sectionName
-                yearCount=[]
-                for year in years:
-                    date=datetime.strptime(year, '%Y')
-                    cYear=db.session.execute('SELECT COUNT(*) FROM anon WHERE :beginDate <= pdate AND pdate <= :endDate AND year=:yearInt AND section =:sectionName',{'yearInt':date.year,'beginDate':beginDate, 'endDate':endDate,'sectionName':sectionName}).first()[0]
-                    yearCount.append(cYear)
-                
-                # adding y values 
-                graph.add(Sname, yearCount)
+                constructGraph("%Y", years, "SELECT COUNT(*) FROM anon WHERE :beginDate <= pdate AND pdate <= :endDate AND year=:yearInt",{'beginDate':beginDate, 'endDate':endDate},'NYT',graph)
+            else:
+                for sectionName in section_name_Chosen:
+                    constructGraph("%Y", years, "SELECT COUNT(*) FROM anon WHERE :beginDate <= pdate AND pdate <= :endDate AND year=:yearInt AND section =:sectionName",{'beginDate':beginDate, 'endDate':endDate,'sectionName':sectionName},'NYT-' + sectionName,graph)
     
-        
-
     graph_data = graph.render_data_uri()
 
     return render_template('drinker.html', count = count, graph_data=graph_data, query=query)
+
+def constructGraph(scale, x_labels, query,params,Sname, graph):
+    count= counter(scale, x_labels, query,params)
+    graph.x_labels= x_labels
+    graph.add(Sname,count)
+
+
+def counter(scale, x_labels, query,params):
+    counter=[]
+    for x in x_labels:
+        print(x)
+        date=datetime.strptime(x, scale)
+        params['monthInt']=date.month
+        params['yearInt']=date.year
+        count=executeQuery(query,params).first()[0]
+        counter.append(count)
+    return counter
 
 @app.route("/getPlotCSV/<query>")
 def get_branch_data_file(query):
@@ -192,39 +166,7 @@ def get_branch_data_file(query):
     datal = list(data.fetchall())
     return render_template('csvoutput.html', data=datal)
 
-    '''
-    (file_basename, server_path, file_size) = create_csv(data)
-    return_file = open(server_path+file_basename, 'r')
-    response = make_response(return_file,200)
-    response.headers['Content-Description'] = 'File Transfer'
-    response.headers['Cache-Control'] = 'no-cache'
-    response.headers['Content-Type'] = 'text/csv'
-    response.headers['Content-Disposition'] = 'attachment; filename=%s' % file_basename
-    response.headers['Content-Length'] = file_size
-    return response
-
-## function to create csv from the SQLAlchemy query response object
-
-
-def create_csv(data):
-    """ returns (file_basename, server_path, file_size) """
-    file_basename = 'output.csv'
-    server_path = '/'
-    #w_file = open(server_path+file_basename,'w+')
-    w_file =open(r"/output.csv","w+")
-    w_file.write('title,link,agency,day,month,year,phrase,section,snip,pdate \n')
-    
-    for row in data:
-        row_as_string = str(row)
-        w_file.write(row_as_string[1:-1] + '\n') ## row_as_string[1:-1] because row is a tuple
-
-    w_file.close()
-
-    w_file = open(server_path+file_basename,'r')
-    file_size = len(w_file.read())
-    return file_basename, server_path, file_size
-
-'''
+ 
 @app.template_filter('pluralize')
 def pluralize(number, singular='', plural='s'):
     return singular if number in (0, 1) else plural
