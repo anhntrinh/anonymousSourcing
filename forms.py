deafda3887ccab97a23ebd899ca48436f09f4faf
Anh Trinh <ant19@duke.edu>
2016-12-14 22:32:28 -0500

    Update forms.py
    final version

--- forms.py
+++ forms.py
@@ -6,7 +6,7 @@ from datetime import datetime,date
 class AnonymousFilterForm(FlaskForm):
 
     beginDate  = DateField('beginDate', format = '%m/%d/%Y', default = date(2000,01,01))
-    endDate = DateField('endDate', format='%m/%d/%Y',default = date(2016,10,10))
+    endDate = DateField('endDate', format='%m/%d/%Y',default = date(2016,11,10))
     sectionMenu = SelectMultipleField('sectionMenu',choices=[],coerce = int)
     scale = SelectField('scale',[optional()],choices=[(1,'Month'), (2,'Year')],coerce = int,default = [1])
 
